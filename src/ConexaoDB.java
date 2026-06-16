18de326a8bb74e4d47ca7673debe809e27da9d89
Matheus Felipe <marcos.junior357@hotmail.com>
2026-06-16 15:45:47 -0300

    "Estrutura de Estoque com JavaFX, DAO,Sqlite"

---
+++ src/ConexaoDB.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDB {
+    private static final String URL_JDBC_PADRAO = "jdbc:sqlite:meu_banco_de_dados.db";
+
+    public static Connection conectar() {
+         try {
+             return DriverManager.getConnection(url:"jdbc:sqlite:meu_banco_de_dados.db");
+         } catch (SQLException var1) {
+             System.err.println("Erro ao conectar ao banco de dados: " + var1.getMessage());
+             return null;
+         }
+    }
+
+    public static Connection conectarGenerico(String var0, String var1, String var2) {
+        try {
+            return DriverManager.getConnection(var0, var1, var2);
+        } catch (SQLException var4) {
+            System.err.println("Erro ao conectar ao banco de dados: " + var4.getMessage());
+            return null;
+        }
+    }
+}
